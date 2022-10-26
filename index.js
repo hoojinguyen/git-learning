55d79b06fa21bfaacd78542b71d5e3ff47321754
HooJi Nguyen <hoojinguyen@HooJis-MacBook-Pro.local>
2022-10-26 11:50:29 +0700

    FIRST

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log("first update");
 console.log("seccond update");
 console.log("third update");
+
+
+console.log("COMBAI 1 - index");
--- index2.js
+++ index2.js
@@ -1,3 +1,6 @@
 console.log("FIRST");
 console.log("SECCOND");
 console.log("THIRD");
+
+
+console.log("COMBAI 1 - index2");
