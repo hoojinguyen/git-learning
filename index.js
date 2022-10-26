57acc02352470b6b4c4f04f8138227cceca06508
Hoi Nguyen Van <vanhoinguyen98@gmail.com>
2022-10-26 11:55:52 +0700

    Combine Branch (#4)
    * FIRST
    
    * SECCOND
    
    * Something else

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("first update");
 console.log("seccond update");
 console.log("third update");
+
+
+console.log("COMBAI 1 - index");
+console.log("COMBAI 2 - index");
--- index2.js
+++ index2.js
@@ -1,3 +1,7 @@
 console.log("FIRST");
 console.log("SECCOND");
 console.log("THIRD");
+
+
+console.log("COMBAI 1 - index2");
+console.log("COMBAI 2 - index2");
